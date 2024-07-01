9c93aeffaac12c7baffdae7cda2b6960ef97be02
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-01 11:52:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+const http = require("http");
+const url = require("url");
+const axios = require("axios");
+
+
+
+// Function to make a GET request with query parameters using axios
+async function makeApiCall(queryParams) {
+    const response = await axios.get('http://api.example.com/endpoint', {  // Replace with your API's URL
+      params: queryParams
+    });
+    return response.data;
+  }
+
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  // Write some text to the response
+  res.end("Welcome to my simple Node.js appsssss!");
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
