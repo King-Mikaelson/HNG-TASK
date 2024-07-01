5f4e6a021126b19ae513f1dd1c3cb19ab4c97124
King-Mikaelson <anazodomichael27@gmail.com>
2024-07-01 12:10:05 +0100

    mike

--- index.js
+++ index.js
@@ -15,31 +15,24 @@ async function makeApiCall(queryParams) {
 
 
 // Create a server object
-const server = http.createServer(async (req, res) => {
+const server = http.createServer(async(req, res) => {
     if (req.url.startsWith('/api')) {
         const queryParams = url.parse(req.url, true).query;
-
-        console.log(queryParams, req.ip)
-
+    
+        try {
+          const apiResponse = await makeApiCall(queryParams);
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(apiResponse));
+        } catch (error) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Error calling API");
+        }
+      } else {
+        // Set the response header
         res.writeHead(200, { "Content-Type": "text/plain" });
         // Write some text to the response
-        res.end(`Welcome to my simple Node.js app!`);
-    
-        // try {
-        //   const apiResponse = await makeApiCall(queryParams);
-        //   res.writeHead(200, { "Content-Type": "application/json" });
-        //   res.end(JSON.stringify(apiResponse));
-        // } catch (error) {
-        //   res.writeHead(500, { "Content-Type": "text/plain" });
-        //   res.end("Error calling API");
-        // }
-      } 
-    //   else {
-    //     // Set the response header
-    //     res.writeHead(200, { "Content-Type": "text/plain" });
-    //     // Write some text to the response
-    //     res.end("Welcome to my simple Node.js app!");
-    //   }
+        res.end("Welcome to my simple Node.js app!");
+      }
 });
 
 // Define the port to listen on
